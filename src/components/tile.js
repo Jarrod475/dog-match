7c768ce45dcf6b2c81bb3a04c38eb014e31cb6d6
Jarrod475 <jarrodlondtwork@gmail.com>
2024-12-13 14:13:52 +0200

    Add 'hidden' SVG to hide tiles. add functions for shuffling and delay.

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,8 +1,23 @@
+import secret from './SVG/secret.svg'
+import {useState} from 'react';
 
 function Tile(props){
+
+    //depending the state of our button tile dog thing, its either the 'hidden' SVG or its the img of its respective woofie.
+    const [imgVisible,setState] = useState(false);
+    let isSolved = false;
+
+    function clicked(){
+        if(!imgVisible){
+            setState(!imgVisible);
+            props.tileSelector(props.pairValue);
+        }   
+    }
+
+
     return(
-        <button className="tile">
-            <img src={props.img} alt="A cute dog of some sort..." />
+        <button onClick={clicked} className="tile">
+           {imgVisible === true?<img src={props.img} alt="A pup undercover!" />:<img src={secret} alt="A cute dog of some sort..." /> } 
         </button>
     )
 }
--- src/components/tilebox.js
+++ src/components/tilebox.js
@@ -1,16 +1,49 @@
 import Tile from "./tile";
-
+//import {useState,useEffect} from "react";
 
 function Tilebox(props){
 
+
+
+
+    let newArray = [];
+    for (let x= 0; x< props.imgURL.length; x++){
+        newArray.push(x);
+        newArray.push(x);
+       
+    }
+    newArray = ShuffleArray(newArray);
+      
+    
+
+    //this fucntion recieves the clicked button's Pair ID
+    function TileClicked(ID){
+        console.log("TIle id is:",ID);
+    }
+
+
+
     return(
         <div className="tilecontainer">
-            {props.imgURL.length > 0 && props.imgURL.map((URL,index)=>{
-                return <Tile key={index} img={URL} />})}
+            {newArray.length > 0 && newArray.map((key,index)=>{
+                return (
+                    <Tile 
+                        key={index} 
+                        img={props.imgURL[key]} 
+                        pairValue={key} 
+                        tileSelector = {TileClicked}
+                        />);})}
         </div>
     )
 }
 
+//this function is like await().
+ // sleep(2000).then(() => { });
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+//this shuffles the array using the 'fisher yates' method
 function ShuffleArray(array){
     for (let i = array.length - 1; i >= 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
